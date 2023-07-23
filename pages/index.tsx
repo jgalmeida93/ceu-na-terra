fafc2801ca717a85c832ef8c8d2172b01567d707
Jonas Gabriel <jgalmeida1993@gmail.com>
2023-07-22 23:15:56 -0300

    :fire: (console) remove console.log

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,8 +130,6 @@ export async function getStaticProps() {
     .execute();
   let reducedResults: ImageProps[] = [];
 
-  console.log(results);
-
   let i = 0;
   for (let result of results.resources) {
     reducedResults.push({
